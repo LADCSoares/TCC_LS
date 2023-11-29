b71e3cb937a93418184ecf56474156c9afe5b12e
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-11-29 11:55:24 -0300

    Create cadFuncionario.php

---
+++ cadFuncionario.php
@@ -0,0 +1,21 @@
+<?php
+// faz conexao com o banco
+include("conecta.php");
+
+$nome = $_POST["nome"];
+$cpf = $_POST["cpf"];
+// var_dump($cpf);
+$cargo = $_POST["cargo"];
+$telefone = $_POST["telefone"];
+// var_dump($telefone);
+
+$sql = "INSERT INTO funcionario (nome, cpf, cargo, telefone) VALUES ('$nome', '$cpf' , '$cargo', '$telefone')"; // pede para inserir em tal tabela no banco
+$resultado = mysqli_query($conexao, $sql);
+
+if ($resultado == true) {
+    header("Location: consultarFun.php?result=alert");
+}else{
+    header("Location: formCadFun.php?result=false");
+}
+
+?>
